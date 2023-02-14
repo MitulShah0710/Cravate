db5e6e21184a2186dd7c5a10d8b4cdc2c5899893
mitul <mitulshah141516@gmail.com>
2023-02-14 05:46:44 -0400

    Connection to DB code

--- db-connection/connection.js
+++ db-connection/connection.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
+const URL = require("./connectionString");
 
+const connection = function () {
+    return new Promise((resolve, reject) => {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(URL, (error, result) => {
+            if(error) {
+                return reject(error, "error")
+            }
+            console.log("DB connected successfully");
+            return resolve('success');
+        });
+    });
+}
+
+module.exports = {
+    connect: connection
+}
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const connection = require("./db-connection/connection");
 var app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
+const server = require('http').createServer(app);
+
+connection.connect().then(success => {
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
 app.get('/initial', function (req, res) {
     res.json("Hello World");
 });
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
 
 module.exports = app;
